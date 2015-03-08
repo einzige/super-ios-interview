2ea5f112176ba353a6195c896f70873fc933adce
Sergei Zinin <zinin@xakep.ru>
2015-03-09 00:19:03 +0700

    Refactor list

--- Connectpal/PostsViewController.swift
+++ Connectpal/PostsViewController.swift
@@ -8,10 +8,19 @@
 
 import UIKit
 
-class PostsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class PostsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, APIControllerProtocol {
     @IBOutlet var postsTableView: UITableView?
     
     var postsData = []
+    var api = APIController()
+    
+    func didReceiveAPIResults(results: NSDictionary) {
+        var resultsArr: NSArray = results["results"] as NSArray
+        dispatch_async(dispatch_get_main_queue(), {
+            self.postsData = resultsArr
+            self.postsTableView?.reloadData()
+        })
+    }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return postsData.count
@@ -43,47 +52,14 @@ class PostsViewController: UIViewController, UITableViewDataSource, UITableViewD
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchItunesFor("JQ Software")
+        api.delegate = self
+        api.searchItunesFor("JQ Software")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    func searchItunesFor(searchTerm: String) {
-        
-        // The iTunes API wants multiple terms separated by + symbols, so replace spaces with + signs
-        let itunesSearchTerm = searchTerm.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.CaseInsensitiveSearch, range: nil)
-        
-        // Now escape anything else that isn't URL-friendly
-        if let escapedSearchTerm = itunesSearchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
-            let urlPath = "http://itunes.apple.com/search?term=\(escapedSearchTerm)&media=software"
-            let url = NSURL(string: urlPath)
-            let session = NSURLSession.sharedSession()
-            let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
-                println("Task completed")
-                if(error != nil) {
-                    // If there is an error in the web request, print it to the console
-                    println(error.localizedDescription)
-                }
-                var err: NSError?
-                
-                var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSDictionary
-                if(err != nil) {
-                    // If there is an error parsing JSON, print it to the console
-                    println("JSON Error \(err!.localizedDescription)")
-                }
-                let results: NSArray = jsonResult["results"] as NSArray
-                dispatch_async(dispatch_get_main_queue(), {
-                    self.postsData = results
-                    self.postsTableView!.reloadData()
-                })
-            })
-            
-            task.resume()
-        }
     }
 }
 
