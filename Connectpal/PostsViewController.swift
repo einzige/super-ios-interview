539b2a85a73a6a9f8a725ed19e81f8f9102a7c5f
Sergei Zinin <zinin@xakep.ru>
2015-03-09 01:54:46 +0700

    Better closure syntax

--- Connectpal/PostsViewController.swift
+++ Connectpal/PostsViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Connectpal
-//
-//  Created by Sergei Zinin on 06/03/15.
-//  Copyright (c) 2015 Connectpal. All rights reserved.
-//
-
 import UIKit
 
 class PostsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
@@ -43,22 +35,21 @@ class PostsViewController: UIViewController, UITableViewDataSource, UITableViewD
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        api.onDataReceived() {
-            var results = $0
+        
+        api.onDataReceived({ (results: NSDictionary) in
             var resultsArr: NSArray = results["results"] as NSArray
+            
             dispatch_async(dispatch_get_main_queue(), {
                 self.postsData = resultsArr
                 self.postsTableView?.reloadData()
             })
-        }
+        })
         
         api.searchItunesFor("JQ Software")
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning();
     }
 }
 
