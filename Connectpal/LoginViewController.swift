b4ddc2b744618db7d2db6a96423dd45e848a8d0a
Sergei Zinin <zinin@xakep.ru>
2015-03-11 22:35:27 +0700

    Improve email password resignments

--- Connectpal/LoginViewController.swift
+++ Connectpal/LoginViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var logInButton: UIButton!
@@ -20,6 +20,26 @@ class LoginViewController: UIViewController {
         sessionManager.signIn(emailField.text, password: passwordField.text,
             onSuccess: onLoginSuccess,
             onFail: onLoginFailed)
+    }
+    
+    override func viewDidLoad() {
+        emailField.delegate = self
+        passwordField.delegate = self
+        emailField.canBecomeFirstResponder()
+        passwordField.canBecomeFirstResponder()
+        api.resetSession()
+        super.viewDidLoad()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if (textField == passwordField) {
+            passwordField.resignFirstResponder()
+            login()
+        } else if (textField == emailField) {
+            passwordField.becomeFirstResponder()
+        }
+        
+        return true
     }
     
     private func onLoginSuccess() {
@@ -57,10 +77,5 @@ class LoginViewController: UIViewController {
         passwordField.text = ""
         logInButton.enabled = true
         logInButton.titleLabel!.text = "Log In"
-    }
-    
-    override func viewDidLoad() {
-        api.resetSession()
-        super.viewDidLoad()
     }
 }
