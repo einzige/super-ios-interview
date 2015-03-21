1e8b055d595587702482f3b9de9e9f0ce73a08f8
Sergei Zinin <zinin@xakep.ru>
2015-03-21 12:01:37 +0700

    Add my feed controller

---
+++ Connectpal/FeedViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class FeedViewController: UICollectionViewController {
+    
+    @IBOutlet weak var loader: UIActivityIndicatorView!
+    
+    override func viewDidAppear(animated: Bool) {
+        
+    }
+    
+    func postsLoaded() {
+        
+    }
+}
