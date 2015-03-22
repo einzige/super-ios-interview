af02142735655ba5e772708973e238d4eb4732b9
Sergei Zinin <zinin@xakep.ru>
2015-03-22 21:13:19 +0700

    Display profile images in the feed

--- Connectpal/FeedViewController.swift
+++ Connectpal/FeedViewController.swift
@@ -4,13 +4,15 @@ class FeedViewController: UICollectionViewController, UICollectionViewDelegateFl
     @IBOutlet weak var loader: UIActivityIndicatorView!
     
     var posts: [AnyObject] = [AnyObject]()
+    var profileImageCache = [Int: UIImage]()
     
     override func viewDidAppear(animated: Bool) {
         { return api.feed() } ~> postsLoaded
     }
     
     func postsLoaded(response: APIResponse) {
-        
+        self.posts = response.getData()["posts"] as [AnyObject]
+        self.collectionView?.reloadData()
     }
     
     // UICOllectionViewDelegateFlowLayout
@@ -24,10 +26,55 @@ class FeedViewController: UICollectionViewController, UICollectionViewDelegateFl
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("post-cell", forIndexPath: indexPath) as PostCell
-        
         let post: [String: AnyObject] = posts[indexPath.row] as [String: AnyObject]
+        
         cell.postTitle?.text = post["title"] as? String
+        cell.postMessage?.text = post["message"] as? String
+        cell.postTime?.text = post["created_at"] as? String
+        
+        let authorData = post["user"] as [String: AnyObject]
+        let authorID = authorData["id"] as Int
+        var image = profileImageCache[authorID]
+        
+        if image == nil {
+            // If the image does not exist, we need to download it
+            var imgURL: NSURL = NSURL(string: authorData["small_profile_picture_url"] as String)!
+            
+            // Download an NSData representation of the image at the URL
+            let request: NSURLRequest = NSURLRequest(URL: imgURL)
+            
+            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler: {(response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+                if error == nil {
+                    image = UIImage(data: data)
+                    
+                    // Store the image into the cache
+                    self.profileImageCache[authorID] = image! as UIImage
+                    dispatch_async(dispatch_get_main_queue(), {
+                        if let cellToUpdate = collectionView.cellForItemAtIndexPath(indexPath) as? PostCell {
+                            cellToUpdate.postImage?.image = image
+                        }
+                    })
+                }
+                else {
+                    println("Error: \(error.localizedDescription)")
+                }
+            })
+        } else {
+            dispatch_async(dispatch_get_main_queue(), {
+                if let cellToUpdate = collectionView.cellForItemAtIndexPath(indexPath) as? PostCell {
+                    cellToUpdate.postImage?.image = image
+                }
+            })
+        }
         
         return cell
+    }
+    
+    private func setImageAt(indexPath: NSIndexPath, image: UIImage) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if let cellToUpdate = (self.view as UICollectionView).cellForItemAtIndexPath(indexPath) as? PostCell {
+                cellToUpdate.postImage?.image = image
+            }
+        })
     }
 }
--- Connectpal/LoginViewController.swift
+++ Connectpal/LoginViewController.swift
@@ -1,11 +1,3 @@
-//
-//  LoginViewController.swift
-//  Connectpal
-//
-//  Created by Sergei Zinin on 10/03/15.
-//  Copyright (c) 2015 Connectpal. All rights reserved.
-//
-
 import UIKit
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
