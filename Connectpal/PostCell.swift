c41f674d3a08eea69e8bf6815f001ab3a4c04255
Sergei Zinin <zinin@xakep.ru>
2015-03-26 14:07:34 +0700

    Remove shadows

--- Connectpal/PostCell.swift
+++ Connectpal/PostCell.swift
@@ -18,10 +18,10 @@ class PostCell: UICollectionViewCell {
         super.init(coder: aDecoder)
         layer.cornerRadius = 3
         
-        layer.masksToBounds = false
-        layer.shadowOpacity = 0.1
-        layer.shadowRadius = 2.0
-        layer.shadowOffset = CGSize(width: 1, height: 1)
+        //layer.masksToBounds = false
+        //layer.shadowOpacity = 0.1
+        //layer.shadowRadius = 2.0
+        // layer.shadowOffset = CGSize(width: 1, height: 1)
         //layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: 5).CGPath
         //layer.shouldRasterize = true
     }
